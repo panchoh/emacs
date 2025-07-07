0776d05b6f2638b23a29186c3079bf10e0db0033
pancho horrillo <pancho@pancho.name>
2025-07-07 17:10:16 +0200

    feat(init): prepare for use-general
    https://github.com/progfolio/elpaca/wiki/Migrating-to-Elpaca#asynchronicity

--- init.el
+++ init.el
@@ -73,6 +73,12 @@
         ;; Enable use-package :ensure support for Elpaca.
         (elpaca-use-package-mode))
 
+;; Block until package is installed/activated so we can use it at the top-level below.
+(elpaca-wait)
+;; Now we can use the :ensure use-package keyword.
+;; You'll want to call `elpaca-wait` after other packages which are required at the top-level of your init.
+;; For example, general.el is a popular keybinding library which adds its own use-package keyword.
+
 ;;When installing a package used in the init file itself,
 ;;e.g. a package which adds a use-package key word,
 ;;use the :wait recipe keyword to block until that package is installed/configured.
